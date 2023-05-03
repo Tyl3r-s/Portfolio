8329365fec5c70850fd93442adedacc9e92cacb2
Tyler <Tyl3r.s@outlook.com>
2023-05-02 21:12:03 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/pages/About';
 import Work from './components/pages/Work';
 import Contact from './components/pages/Contact';
 import Skills from './components/pages/Skills';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
